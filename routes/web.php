c8593dc085dccd259e416ec38dca7dc04f0c8909
TaiwanJohn <53088692+john8895@users.noreply.github.com>
2021-09-22 16:14:07 +0800

    mysql config setting

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Route::get('/', function (){
+//    return 'this is a book';
+//});
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
